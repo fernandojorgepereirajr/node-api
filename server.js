9e8ccf07c9ae8fd9508ef38da4aee4abd4594992
Fernando Jorge <fernando.junior@ccc.ufcg.edu.br>
2020-04-10 16:46:48 -0300

    end-t

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
 
 // Iniciando app
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 //Iniciando o BD
-mongoose.connect('mongodb://localhost:27017/nodeapi',{ useNewUrlParser: true , useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 requireDir('./src/models')
